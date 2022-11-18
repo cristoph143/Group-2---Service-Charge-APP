9d7ab506269ed33c6a8b992422611d5a54c83d1a
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-11-18 22:38:12 +0800

    Changed login screen UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Group3-Service-Charge';
+  title = 'Group2-Service-Charge';
 }
