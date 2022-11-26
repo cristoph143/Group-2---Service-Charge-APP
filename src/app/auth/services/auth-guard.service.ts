eebdac5b23bbfa4b120f2ca02d4ac442453aa3dc
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-11-26 11:18:04 +0800

    Add AuthGuard

--- src/app/auth/services/auth-guard.service.ts
+++ src/app/auth/services/auth-guard.service.ts
@@ -11,9 +11,11 @@ import { AuthService } from "./auth.service";
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(): Observable<boolean> {
+    console.log(this.authService.isUserLoggedIn$.value);
     if (!this.authService.isUserLoggedIn$.value) {
     this.router.navigate(["/login"]);
   }
+
   return this.authService.isUserLoggedIn$;
 }
 }
