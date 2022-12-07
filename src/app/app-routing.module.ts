0d1ddb1c251cd581fb1023aa6b108466dfcbec49
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-12-08 01:41:10 +0800

    Add authguard to other routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../app/auth/login/login.component';
 import { AuthGuard } from './auth/services/auth-guard.service';
+import { MenuReportsComponent } from './reports/menu-reports/menu-reports.component';
+import { MenuComponent } from './ticket-management/menu/menu.component';
+import { MenuUserRoleComponent } from './user-role-mgt/menu-user-role/menu-user-role.component';
 import { UserDashboardComponent } from './users/user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
@@ -12,8 +15,8 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    // component: LoginComponent,
-    component: UserDashboardComponent,
+    component: LoginComponent,
+    // component: UserDashboardComponent,
   },
   {
     path: 'user-dashboard',
@@ -23,6 +26,39 @@ const routes: Routes = [
       {
         path: 'user-dashboard',
         component: UserDashboardComponent
+      },
+    ]
+  },
+  {
+    path: 'ticket-management',
+    component: MenuComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'ticket-management',
+        component: MenuComponent,
+      },
+    ]
+  },
+  {
+    path: 'reports',
+    component: MenuReportsComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'reports',
+        component: MenuReportsComponent,
+      },
+    ]
+  },
+  {
+    path: 'user-role-management',
+    component: MenuUserRoleComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'user-role-management',
+        component: MenuUserRoleComponent,
       },
     ]
   },
